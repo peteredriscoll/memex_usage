e753be6eb63e31ff3f2ed4ee32dd87d0c7fa4b80
peterdriscollciw <pdriscoll@ciw.edu>
2020-03-09 21:27:05 -0400

    improved fig 1

--- plot_usage.py
+++ plot_usage.py
@@ -24,9 +24,14 @@ for i in range(nbins-1): cpu_mids[i]=np.mean(cpu_bins[i:i+1])
 cpu_mids[nbins-1]=np.mean([121,3e3])
 dept = 'dtm'
 p.figure()
+ax=p.gca()
+formatter = mdates.DateFormatter("%y-%m-%d")
+ax.xaxis.set_major_formatter(formatter)
+locator = mdates.DayLocator()
+ax.xaxis.set_major_locator(locator)
 for i_bin in range(nbins):
     xvalues = [datetime.datetime.strptime(d,"%m/%d/%y").date() for d in weeks]
-    p.plot(weeks,np.array(cpuhours[dept])[:,i_bin],label=bin_labels[i_bin])
+    p.plot(xvalues,np.array(cpuhours[dept])[:,i_bin].astype(float),label=bin_labels[i_bin])
 p.xlabel('week')
 p.ylabel('cpu hours')
 p.legend(loc='best')
@@ -35,8 +40,8 @@ p.legend(loc='best')
 ### Plot cpu hours vs N cpu
 p.figure()
 for i_bin in range(nbins):
-    #p.plot(np.zeros(nweeks)+cpu_bins[i_bin],np.array(cpuhours[dept])[:,i_bin].astype(float),'o',label=dept)
+    p.plot(np.zeros(nweeks)+cpu_bins[i_bin],np.array(cpuhours[dept])[:,i_bin].astype(float),'o',label=dept)
     ydata=np.array(cpuhours[dept])[:,i_bin].astype(float)
     #p.errorbar(cpu_mids[i_bin],np.mean(ydata),label=dept,yerr=np.std(ydata))
-    p.bar(cpu_mids[i_bin],np.mean(ydata),width=cpu_mids[i_bin]-cpu_bins[i_bin],align='center')
+    #p.bar(cpu_mids[i_bin],np.mean(ydata),width=cpu_mids[i_bin]-cpu_bins[i_bin],align='center')
 p.legend(loc='best')
